06645cba6f7d9482da3507aa899cd83a4d29e5fb
adrianjz02 <adr.jimenez.ian@gmail.com>
2024-12-04 11:32:58 +0100

    commit

---
+++ src/app/liste/liste.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste',
+  imports: [],
+  templateUrl: './liste.component.html',
+  styleUrl: './liste.component.css'
+})
+export class ListeComponent {
+
+}
