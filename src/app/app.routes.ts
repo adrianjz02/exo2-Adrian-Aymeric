f47944e12c2a4d4b7c1f161af35c3a1cedd8bb32
adrianjz02 <adr.jimenez.ian@gmail.com>
2024-12-04 11:31:59 +0100

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AccueilComponent} from './accueil/accueil.component';
+import {GestionComponent} from './gestion/gestion.component';
+import {ListeComponent} from './liste/liste.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'accueil',
+    component: AccueilComponent,
+    children: [
+      { path: 'accueil-child', component: AccueilComponent }
+    ]
+  },
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'gestion', component: GestionComponent},
+  {path: 'liste', component: ListeComponent},
+  {path: '', redirectTo: '/accueil', pathMatch: 'full'}, // redirige vers  l'url /accueil les url vide
+  {path: '**', component: AccueilComponent} // redirige vers l'url /accueil les url inconnues
+];
